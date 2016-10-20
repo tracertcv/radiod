405f28464fed745dc2ba5917900073ca24331f82
cquinlan <yayforeverything@gmail.com>
2016-10-20 16:15:06 -0700

    Origin push

--- Rabbot/Program.cs
+++ Rabbot/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 
 namespace Rabbot
 {
@@ -10,6 +11,7 @@ namespace Rabbot
     {
         static void Main(string[] args)
         {
+            myBot bot = new myBot();
         }
     }
 }
